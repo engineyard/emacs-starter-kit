e62cac90b9d3daea4fa31d03739c1c507896edb7
Sam Merritt <spam@andcheese.org>
2009-06-18 12:00:32 -0700

    Support Sam's Clojure habit.
    Interacting with Clojure via SLIME now works. Since the changes are in 'sam.el',
    they'll only take effect for users or machines named 'sam'. In particular,
    pairing machines will not be affected.

---
+++ sam.el
@@ -0,0 +1,10 @@
+;; clojure + slime
+(add-to-list 'load-path "~/.emacs.d/vendor/swank-clojure")
+(require 'swank-clojure-autoload)
+(swank-clojure-config
+ (setq swank-clojure-jar-path "~/clojure/clojure.jar")
+ (setq swank-clojure-extra-classpaths '("~/clojure-contrib/clojure-contrib.jar" "~/euler/lib")))
+
+(add-to-list 'load-path "~/.emacs.d/vendor/slime")
+(require 'slime)
+(slime-setup)
