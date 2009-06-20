fb21047778677bc2782da0a43af6bea74f0e6ee8
Sam Merritt <spam@andcheese.org>
2009-06-19 18:11:35 -0700

    Let Clojure's JVM have some memory

--- sam.el
+++ sam.el
@@ -2,6 +2,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/swank-clojure")
 (require 'swank-clojure-autoload)
 (swank-clojure-config
+ (setq swank-clojure-extra-vm-args '("-Xmx1g"))
  (setq swank-clojure-jar-path "~/clojure/clojure.jar")
  (setq swank-clojure-extra-classpaths '("~/clojure-contrib/clojure-contrib.jar" "~/euler/lib")))
 
