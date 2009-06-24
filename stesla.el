79876091194dabfebdbd965c7f4d69b32587f9a1
Samuel Tesla <stesla@engineyard.com>
2009-06-23 22:05:40 -0500

    Added support for select key behavior I like.

--- stesla.el
+++ stesla.el
@@ -1,1 +1,13 @@
 (color-theme-twilight)
+
+;; keen select mode
+(add-to-list 'load-path "~/.emacs.d/vendor/keen")
+(require 'keen-select)
+
+(keen-select-define-key user-init-file "."
+  (find-buffer-visiting user-init-file)
+  (find-file user-init-file))
+
+(keen-select-define-key shell "!"
+  (find-buffer-visiting "*eshell*")
+  (eshell))
