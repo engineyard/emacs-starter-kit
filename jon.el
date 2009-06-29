3cf67c15a94cc811eace50113735ff24c9b7a8ef
Jon Crosby <jon@joncrosby.me>
2009-06-28 18:06:20 -0700

    Wire in erlang and distel

--- jon.el
+++ jon.el
@@ -1,7 +1,18 @@
+;; color-theme
+;; use M-x color-theme-select to see options
 (add-to-list 'load-path "~/.emacs.d/vendor/color-theme")
 (require 'color-theme)
+
+;; erlang mode
 (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.4/emacs" load-path))
 (setq erlang-root-dir "/usr/local/lib/erlang")
 (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
 (require 'erlang-start)
+
+;; distel
+(let ((distel-dir "~/.emacs.d/vendor/distel/elisp"))
+  (unless (member distel-dir load-path)
+    (setq load-path (append load-path (list distel-dir)))))
+(require 'distel)
+(distel-setup)
 
