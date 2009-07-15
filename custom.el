4953d11d1eaf6019f479b17470686896aca93b2e
Sam Merritt <spam@andcheese.org>
2009-07-15 11:49:31 -0700

    never mind

--- custom.el
+++ custom.el
@@ -32,7 +32,6 @@
 ;; haml
 (require 'haml-mode nil t)
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
-(add-hook 'haml-mode-hook 'coding-hook)
 
 ;; just nice to have everywhere
 (add-hook 'coding-hook (lambda () (setq tab-width 2)))
