0de899f91df91aaece0c3a4b05c7068c646c5bc7
Samuel Tesla <stesla@engineyard.com>
2009-07-30 13:00:23 -0500

    Remove keen-select stuff

--- stesla.el
+++ stesla.el
@@ -7,18 +7,6 @@
 ;; I actually don't like auto-fill most of the time.
 (remove-hook 'text-mode-hook 'turn-on-auto-fill)
 
-;; keen select mode
-(add-to-list 'load-path "~/.emacs.d/vendor/keen")
-(require 'keen-select)
-
-(keen-select-define-key user-init-file "."
-  (find-buffer-visiting user-init-file)
-  (find-file user-init-file))
-
-(keen-select-define-key shell "!"
-  (find-buffer-visiting "*eshell*")
-  (eshell))
-
 ;; Enable nice display of column and line numbers in the modeline
 (column-number-mode)
 
