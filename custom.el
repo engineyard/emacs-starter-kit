165172722890b7a97216a2f1b54befde612824b9
Sam Merritt <spam@andcheese.org>
2009-07-30 16:45:49 -0700

    color spec output

--- custom.el
+++ custom.el
@@ -100,6 +100,24 @@
   (eval-buffer)
   (expectations-execute))
 
+;;;;;;;; color spec output
+(defadvice compilation-filter (before ansify-compilation-output activate)
+  (with-current-buffer (process-buffer (ad-get-arg 0))
+    (let ((colorstr (ansi-color-apply (ad-get-arg 1))))
+      (ad-set-arg 1 (font-lock-proof colorstr 0)))))
+
+(defun font-lock-proof (string start)
+  (cond
+   ((>= start (length string)) "")
+   (t
+    (let* ((end (next-property-change start string (length string)))
+           (s (substring string start end)))
+      (set-text-properties 0
+                           (length s)
+                           (substitute 'font-lock-face 'face (text-properties-at 0 s))
+                           s)
+      (concat s (font-lock-proof string end))))))
+
 ;;;;;;;; feature (cucumber) mode
 (add-to-list 'load-path "~/.emacs.d/vendor/cucumber")
 (require 'feature-mode)
