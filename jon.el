0678b580d9a9ad839870ecbd43f973c95400c4bf
Jon Crosby <jon@joncrosby.me>
2009-09-22 23:23:18 -0700

    Stop using erlang mode and distel

--- jon.el
+++ jon.el
@@ -1,15 +1,2 @@
-;; erlang mode
-(setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.4/emacs" load-path))
-(setq erlang-root-dir "/usr/local/lib/erlang")
-(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-(require 'erlang-start)
-
-;; distel
-(let ((distel-dir "~/.emacs.d/vendor/distel/elisp"))
-  (unless (member distel-dir load-path)
-    (setq load-path (append load-path (list distel-dir)))))
-(require 'distel)
-(distel-setup)
-
 (color-theme-twilight)
 
