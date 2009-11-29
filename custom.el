1a4d57504ab9a489e25b724489b37f9657ff8c72
Sam Merritt <spam@andcheese.org>
2009-11-28 20:28:42 -0800

    Ignore more stuff when grepping

--- custom.el
+++ custom.el
@@ -183,3 +183,17 @@
 (require 'windmove)
 (windmove-default-keybindings 'super)
 
+;; NB: grep-find-ignored-directories is not limited to just directories.
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "gems" "coverage" "log" "*.dump" "portage.txt" "TAGS"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
