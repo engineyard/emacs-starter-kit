c81a8e766d8833e98f2002de6a29c1a679b7ec1b
Andy Delcambre <adelcambre@engineyard.com>
2010-05-11 16:23:57 -0700

    Make yaml mode work again

--- custom.el
+++ custom.el
@@ -216,3 +216,6 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
+;; yaml-mode is broken in starter kit
+(require 'yaml-mode)
